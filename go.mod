32d6168fea4dfd9a17b709b4f1592fe6895b4152
neosu <neo@neobaran.com>
2021-11-24 23:51:17 +0800

    update ffapp api

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/bwmarrin/snowflake v0.3.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-redis/redis/v8 v8.11.4
-	github.com/gogf/gf v1.16.6
-	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/smartystreets/goconvey v1.7.2
+	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023 // indirect
 )
---
+++ utils/ffapp/token.go
@@ -0,0 +1,55 @@
+package ffapp
+
+import (
+	"fmt"
+
+	"github.com/szpnygo/mab/utils/httpx"
+)
+
+const (
+	// 获取access_token
+	API_FF_APP_ACCESS_TOKEN = "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal"
+)
+
+// RequestAccessToken 请求AccessToken
+func (m *FFApp) RequestAccessToken() (*AccessTokenResponse, error) {
+	request := AccessTokenRequest{
+		AppID:     m.Config.AppID,
+		AppSecret: m.Config.AppSecret,
+	}
+	var response AccessTokenResponse
+
+	err := httpx.Get(API_FF_APP_ACCESS_TOKEN).Struct(&request).JSON(&response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Code != 0 {
+		return nil, fmt.Errorf("%d:%s", response.Code, response.Msg)
+	}
+
+	m.token = response.AppAccessToken
+
+	return &response, nil
+}
+
+func (m *FFApp) SetAccessToken(token string) {
+	m.token = token
+}
+
+func (m *FFApp) GetAccessToken() (string, error) {
+	if len(m.token) > 0 {
+		return m.token, nil
+	}
+
+	accessToken, err := m.RequestAccessToken()
+	if err != nil {
+		return "", err
+	}
+
+	return accessToken.AppAccessToken, nil
+}
+
+func (m *FFApp) AccessToken() {
+	_, _ = m.RequestAccessToken()
+}
